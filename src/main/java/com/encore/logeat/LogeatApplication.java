34acf8c25a9a2c88fbdb5c093621ea3d3d181874
young2866 <62212610+young2866@users.noreply.github.com>
2024-02-02 18:00:02 +0900

    first commit

---
+++ src/main/java/com/encore/logeat/LogeatApplication.java
@@ -0,0 +1,13 @@
+package com.encore.logeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogeatApplication.class, args);
+	}
+
+}
