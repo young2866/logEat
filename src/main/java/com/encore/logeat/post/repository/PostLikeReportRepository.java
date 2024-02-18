c6f2a995ceedbce81e3411359c625367c51b457d
Park Sung Jun <junstin119@gmail.com>
2024-02-19 05:09:24 +0900

    [feat] 주간, 월간 좋아요 상위 리스트 구현 - 게시글의 좋아요 시간을 찍기위한 PostLikeReport 테이블 생성 - 좋아요를 눌렀을 때 PostLikeReport row 추가 - 좋아요를 취소할 떄 PostLikeReport row 삭제 - 주간, 월간 좋아요 DAO 통합 -> 시간으로 가져오기 - 주간, 월간 좋아요 Controller 생성

---
+++ src/main/java/com/encore/logeat/post/repository/PostLikeReportRepository.java
@@ -0,0 +1,33 @@
+package com.encore.logeat.post.repository;
+
+
+import com.encore.logeat.post.domain.PostLikeReport;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface PostLikeReportRepository extends JpaRepository<PostLikeReport, Long> {
+
+//    @Query("SELECT p.post, sum(p.likeCreatedTime) as count " +
+//            "FROM PostLikeReport p " +
+//            "WHERE p.likeCreatedTime between ?1 AND ?2 " +
+//            "GROUP BY p.post ")
+//    Page<PostLikeReport> findByPostLikeReport(LocalDateTime start, LocalDateTime endTime, Pageable pageable);
+
+    @Query("SELECT p " +
+            "FROM PostLikeReport p " +
+            "WHERE p.likeCreatedTime BETWEEN :startDate AND :endDate " +
+            "GROUP BY p.post " +
+            "ORDER BY COUNT(p.likeCreatedTime) DESC ")
+    Page<PostLikeReport> findPostLikeReportBy(@Param("startDate") LocalDateTime startDate,
+                                              @Param("endDate") LocalDateTime endDate, Pageable pageable);
+
+}
--- src/main/java/com/encore/logeat/post/repository/PostRepository.java
+++ src/main/java/com/encore/logeat/post/repository/PostRepository.java
@@ -26,4 +26,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query("SELECT p FROM Post p JOIN p.user u WHERE (p.user.id = :userId OR p.secretYorN = 'N' OR p.secretYorN IS NULL) AND u.nickname LIKE %:userNickname%")
     Page<Post> findByUserNickname(@Param("userId") Long userId, @Param("userNickname") String userNickname, Pageable pageable);
+
+
+
 }
