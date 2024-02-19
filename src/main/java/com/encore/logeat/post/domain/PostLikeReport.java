95651c0ea8c981e84341a0b2ad57609b52e1e536
Park Sung Jun <junstin119@gmail.com>
2024-02-19 14:14:11 +0900

    [feat] 주간, 월간 좋아요 상위 리스트 구현 - @ManyToOne(fetch = FetchType.LAZY) 설정

--- src/main/java/com/encore/logeat/post/domain/PostLikeReport.java
+++ src/main/java/com/encore/logeat/post/domain/PostLikeReport.java
@@ -26,11 +26,11 @@ public class PostLikeReport {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
