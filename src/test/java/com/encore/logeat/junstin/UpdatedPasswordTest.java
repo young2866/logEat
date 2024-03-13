fab3fe3d192d8032acbeb2cd3dd963355c8f7dd6
Park Sung Jun <56985486+J-unStiN@users.noreply.github.com>
2024-03-13 14:03:16 +0900

    시즌 43호 Update UpdatedPasswordTest.java

--- src/test/java/com/encore/logeat/junstin/UpdatedPasswordTest.java
+++ src/test/java/com/encore/logeat/junstin/UpdatedPasswordTest.java
@@ -18,8 +18,6 @@ import javax.persistence.EntityNotFoundException;
 @SpringBootTest
 public class UpdatedPasswordTest {
 
-    @Autowired
-    private EmailService emailService;
 
     @Autowired
     private UserService userService;
@@ -33,27 +31,7 @@ public class UpdatedPasswordTest {
     @Test
     @DisplayName("비밀번호 변경 테스트")
     public void 비밀번호변경() {
-        UserCreateRequestDto createRequestDto = new UserCreateRequestDto();
-        createRequestDto.setEmail("aaaa@naver.com");
-        createRequestDto.setNickname("aaaa");
-        createRequestDto.setPassword("12345");
-        createRequestDto.setProfileImage(null);
-        createRequestDto.setIntroduce("자기소개");
 
-        User user = userService.createUser(createRequestDto);
-        String authNumber = emailService.generateRandomNumber();
-        String emailAuthNumber = emailService.createEmailAuthNumber(user.getEmail(), authNumber);
-
-        Boolean b = emailService.verificationEmailAuth(user.getEmail(), emailAuthNumber);
-
-        User findUser = userRepository.findByEmail(user.getEmail()).orElseThrow(() -> new EntityNotFoundException("아이디가 없습니다."));
-        String changePwd = passwordEncoder.encode("5555");
-
-        if(b) {
-            findUser.userUpdatedPassword(changePwd);
-        }
-
-        Assertions.assertThat(passwordEncoder.matches("5555", findUser.getPassword())).isEqualTo(true);
     }
 
 
